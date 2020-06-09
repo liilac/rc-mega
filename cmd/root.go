c4d8dda0941e4e811c0621c4fd9a304574a23d74
Lilac Kapul <lilac@enby.ninja>
2020-06-09 12:31:31 +1000

    commit initial proof of concept

---
+++ cmd/root.go
@@ -0,0 +1,129 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+var capacityBytes uint64
+var word string
+var metadataSizeBytes uint64
+
+const byteSizeInBits uint64 = 8
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "rc-mega",
+	Short: "Silly application that calculates how many words will fit in a given size",
+	Long: `Exceedingly silly application that calculates how many times a word
+will fit in a given size, using a special compression algorithm.
+
+This algorithm assumes the size required for N copies of word is:
+1KiB (for metadata) + the size of the word + the number N encoded as binary
+
+Another way of putting this is that, for a size S bits you can fit a word of
+size W:
+S - (8 * (1024 + W) )
+times`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func (cmd *cobra.Command, args []string){
+	// calculate number of bits in capacity
+	var capacityBits = capacityBytes * byteSizeInBits
+	// calculate size of word in bytes
+	var wordSize uint64 = uint64(len(word))
+	// calculate word size in bits
+	var wordSizeBits = wordSize * byteSizeInBits
+	// calculate metadata size in bits
+	var metadataSizeBits = metadataSizeBytes & byteSizeInBits
+
+	// calculate remaining bits after metadata and word
+	var availableBits uint64 = capacityBits - (wordSizeBits + metadataSizeBits)
+
+	// calculate max number that will fit in availableBits
+	var numWillFit = Power(2, availableBits)
+
+	// print output
+	fmt.Printf("Guess what! The number of times \"%v\" will fit into %v bytes is: %v\n", word, capacityBytes,
+	numWillFit)
+},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+	rootCmd.PersistentFlags().Uint64VarP(&capacityBytes, "capacity", "c", 1024e12,
+		"number of bytes to fit word into (default is 1024e12 for 1TiB)")
+	rootCmd.PersistentFlags().StringVarP(&word, "word", "w", "sophie", "word to fit into capacity (default is sophie)")
+	rootCmd.PersistentFlags().Uint64VarP(&metadataSizeBytes, "metadata-size", "m", 1024,
+		"metadata size in bytes (default is 1024 for 1KiB)")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".rc-mega" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".rc-mega")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
+
+func Power(a, n uint64) uint64 {
+	var i, result uint64
+	result = 1
+	for i = 0; i < n; i++ {
+		result *= a
+	}
+	return result
+}
